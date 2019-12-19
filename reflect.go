349f205cfd93e488cfb46c2f84856668efe7fbd1
Mikuláš Dítě <dite@mangoweb.cz>
2019-12-19 13:46:54 +0100

    refactor Version to const

--- reflect.go
+++ reflect.go
@@ -19,7 +19,12 @@ import (
 // Version is the JSON Schema version.
 // If extending JSON Schema with custom values use a custom URI.
 // RFC draft-wright-json-schema-00, section 6
-var Version = "http://json-schema.org/draft-04/schema#"
+const Version = VersionDraft4
+const (
+	VersionDraft4 = "http://json-schema.org/draft-04/schema#"
+	VersionDraft6 = "http://json-schema.org/draft-06/schema#"
+	VersionDraft7 = "http://json-schema.org/draft-07/schema#"
+)
 
 // Schema is the root schema.
 // RFC draft-wright-json-schema-00, section 4.5
